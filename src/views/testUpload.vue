6e6289a521abebad79fdb774d58c9de4b7225e9f
Lt <Lt@qq.com>
2024-03-28 18:12:53 +0800

    first commit

--- src/views/testUpload.vue
+++ src/views/testUpload.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
-    <input type="file">
+    <input type="file" @change="onChange">
+    <button @click="submit">提交</button>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 
+let fileObj
 export default {
+    data() {
+        return {
 
+        }
+    }, 
+    methods: {
+        onChange(e) {
+            console.log(e.target.files[0])
+            fileObj = e.target.files
+        },
+        submit() {
+            let formData = new FormData()
+            // formData.append('user', "asd")
+            formData.append('files', fileObj)
+            axios.post("/api/file", formData)
+        }
+    }
 }
 </script>
 
